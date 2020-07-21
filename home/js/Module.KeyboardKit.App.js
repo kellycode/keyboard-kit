bc5b3287e93b89b2732f596a16ce165190c332a0
kellycode <kwkelly@outlook.com>
2020-07-21 14:38:22 -0500

    changed css layout to flex

--- home/js/Module.KeyboardKit.App.js
+++ home/js/Module.KeyboardKit.App.js
@@ -20,36 +20,23 @@ angular.module('KeyboardKit.App',
                 $rootScope.viewChanged = false;
 
                 $scope.handleOrientation = function (win) {
-                    if (win.orientation) {
-                        switch (win.orientation)
-                        {
-                            case - 90:
-                            case 90:
-                                return 'landscape';
-                            default:
-                                return 'portrait';
-                        }
+                    if (window.orientation === 0)
+                    {
+                        return 'portrait';
                     }
-                    else {
-                        if (win.innerHeight > win.innerWidth) {
-                            return 'portrait';
-                        }
-                        else {
-                            return 'landscape';
-                        }
+                    else
+                    {
+                        return 'landscape';
                     }
                 };
 
-                $scope.orientation = $scope.handleOrientation($window);
-
-                // using the parent scope resize broadcast
-                $scope.$on('eventWatcher::resize', function (broadcastEvent, args) {
-                    $timeout(function () {
-                        $scope.orientation = $scope.handleOrientation($window);
-                    }, 0);
+                angular.element($window).on('orientationchange', function (event, args) {
+                    $scope.orientation = $scope.handleOrientation($window);
                 });
 
-
+                angular.element($window).on('load', function (event, args) {
+                    $scope.orientation = $scope.handleOrientation($window);
+                });
 
             }])
         .directive('headerModule', function () {
@@ -66,52 +53,6 @@ angular.module('KeyboardKit.App',
                 replace: true
             };
         })
-        .directive('eventWatcher', ['$window', '$document', function ($window, $document) {
-                return {
-                    link: function (scope) {
-                        angular.element($window).on('resize', function (event, args) {
-                            // namespace the event to avoid conflicts
-                            scope.$broadcast('eventWatcher::resize');
-                        });
-                        angular.element($document).on('keydown', function (event, args) {
-                            scope.$broadcast('eventWatcher::keyaction', event);
-                        });
-                    }
-                };
-            }])
-        .directive('centerInParent', ['$timeout', '$window', function ($timeout, $window) {
-                return{
-                    restrict: 'A', // example use(as attribute) : <span>data-center-in-parent>stuff<span>
-                    link: function (scope, element, attr) {
-                        function centerElement() {
-                            $timeout(function () {
-                                var elem, parent;
-                                elem = {
-                                    width: element[0].clientWidth,
-                                    height: element[0].clientHeight
-                                };
-                                parent = {
-                                    width: element.parent()[0].clientWidth,
-                                    height: element.parent()[0].clientHeight
-                                };
-                                element[0].style.top = (parent.height - elem.height) / 2 + 'px';
-                                element[0].style.left = (parent.width - elem.width) / 2 + 'px';
-                                element[0].style.position = 'absolute';
-                            }, 0);
-                        }
-
-                        angular.element($window).on('resize', function (event, args) {
-                            centerElement();
-                        });
-
-                        scope.$watch(attr.ngShow, function () {
-                            centerElement();
-                        });
-
-                        centerElement();
-                    }
-                };
-            }])
         .directive('appVersion', ['sightReaderConfig', function (sightReaderConfig) {
                 return {
                     restrict: 'A',
--- home/js/Module.KeyboardKit.Directives.js
+++ home/js/Module.KeyboardKit.Directives.js
@@ -29,7 +29,7 @@ angular.module('KeyboardKit.Directives', [])
                     }
                 };
             }])
-        .controller('ModalController', ['$scope',
+        .controller('DialogController', ['$scope',
             function ($scope) {
                 $scope.subjects = [
                     {
@@ -45,4 +45,85 @@ angular.module('KeyboardKit.Directives', [])
                         description: 25
                     }
                 ];
+            }])
+        .directive('resizeDog', ['$timeout', '$window', function ($timeout, $window) {
+                return{
+                    restrict: 'A', // example use(as attribute) : <span>data-center-in-parent>stuff<span>
+                    link: function (scope, element, attr) {
+                        function centerElement() {
+                            $timeout(function () {
+                                var elem, parent;
+                                elem = {
+                                    width: element[0].clientWidth,
+                                    height: element[0].clientHeight
+                                };
+                                parent = {
+                                    width: element.parent()[0].clientWidth,
+                                    height: element.parent()[0].clientHeight
+                                };
+                                element[0].style.top = (parent.height - elem.height) / 2 + 'px';
+                                element[0].style.left = (parent.width - elem.width) / 2 + 'px';
+                                element[0].style.position = 'absolute';
+                            }, 0);
+                        }
+
+                        angular.element($window).on('resize', function (event, args) {
+                            centerElement();
+                        });
+
+                        scope.$watch(attr.ngShow, function () {
+                            centerElement();
+                        });
+
+                        centerElement();
+                    }
+                };
+            }])
+        // NOT USED ATM
+        .directive('eventWatcher', ['$window', '$document', function ($window, $document) {
+                return {//data-event-watcher
+                    link: function (scope) {
+                        angular.element($window).on('resize', function (event, args) {
+                            // namespace the event to avoid conflicts
+                            scope.$broadcast('eventWatcher::resize');
+                        });
+                        angular.element($document).on('keydown', function (event, args) {
+                            scope.$broadcast('eventWatcher::keyaction', event);
+                        });
+                    }
+                };
+            }])
+        // NOT USED ATM
+        .directive('centerInParent', ['$timeout', '$window', function ($timeout, $window) {
+                return{
+                    restrict: 'A', // example use(as attribute) : <span>data-center-in-parent>stuff<span>
+                    link: function (scope, element, attr) {
+                        function centerElement() {
+                            $timeout(function () {
+                                var elem, parent;
+                                elem = {
+                                    width: element[0].clientWidth,
+                                    height: element[0].clientHeight
+                                };
+                                parent = {
+                                    width: element.parent()[0].clientWidth,
+                                    height: element.parent()[0].clientHeight
+                                };
+                                element[0].style.top = (parent.height - elem.height) / 2 + 'px';
+                                element[0].style.left = (parent.width - elem.width) / 2 + 'px';
+                                element[0].style.position = 'absolute';
+                            }, 0);
+                        }
+
+                        angular.element($window).on('resize', function (event, args) {
+                            centerElement();
+                        });
+
+                        scope.$watch(attr.ngShow, function () {
+                            centerElement();
+                        });
+
+                        centerElement();
+                    }
+                };
             }]);
